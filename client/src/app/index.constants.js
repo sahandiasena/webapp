398a94822e4544bc172c332939e47e1a3cc2f76c
Sahan Diasena <sahan@calcey.com>
2017-02-05 14:43:55 +0530

    Updated api url to localhost

--- client/src/app/index.constants.js
+++ client/src/app/index.constants.js
@@ -4,7 +4,7 @@
   angular
     .module('client')
     .constant('config', {
-      apiUrl: 'http://35.166.33.119:4000',
+      apiUrl: 'http://localhost:4000',
       tokenKey: 'token',
       profileKey: 'profile'
     });
